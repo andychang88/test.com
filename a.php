4fd0bce84ed132f78863a1a30541bf9f737ce2c8
andy <changanti@163.com>
2016-11-20 23:05:55 +0800

    m

---
+++ a.php
@@ -0,0 +1,54 @@
+<?php
+
+$url = 'http://v2.erp.moonarstore.com/default/third_party/chanage_code/barcode/html/image.php?code=code128&o=2&t=30&r=1&text=SYBAB03417782&f1=-1&f2=8&a1=&a2=B&a3=';
+echo str_replace("http://v2.erp.moonarstore.com/default/third_party/chanage_code", "http://120.24.100.157:72/default/third_party/chanage_code",$url);
+
+$arr = array("11", "22");
+
+echo "<br>-----------<pre>".__FILE__." ".__LINE__." <br>";print_r($arr);
+
+
+
+$url = 'http://v2.erp.moonarstore.com/default/third_party/chanage_code/barcode/html/image.php?code=code128&o=2&t=30&r=1&text=SYBAB03417782&f1=-1&f2=8&a1=&a2=B&a3=';
+echo str_replace("http://v2.erp.moonarstore.com/default/third_party/chanage_code", "http://120.24.100.157:72/default/third_party/chanage_code",$url);
+
+$arr = array("11", "22");
+
+echo "<br>-----------<pre>".__FILE__." ".__LINE__." <br>";print_r($arr);
+
+
+
+
+
+
+
+
+$url = 'http://v2.erp.moonarstore.com/default/third_party/chanage_code/barcode/html/image.php?code=code128&o=2&t=30&r=1&text=SYBAB03417782&f1=-1&f2=8&a1=&a2=B&a3=';
+echo str_replace("http://v2.erp.moonarstore.com/default/third_party/chanage_code", "http://120.24.100.157:72/default/third_party/chanage_code",$url);
+
+
+echo "<br>-----------<pre>".__FILE__." ".__LINE__." <br>";print_r($arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$url = 'http://v2.erp.moonarstore.com/default/third_party/chanage_code/barcode/html/image.php?code=code128&o=2&t=30&r=1&text=SYBAB03417782&f1=-1&f2=8&a1=&a2=B&a3=';
+
+
+
+echo str_replace("http://v2.erp.moonarstore.com/default/third_party/chanage_code", "http://120.24.100.157:72/default/third_party/chanage_code",$url);
+
+$arr = array("11", "22");
+
+echo "<br>-----------<pre>".__FILE__." ".__LINE__." <br>";print_r($arr);
+
